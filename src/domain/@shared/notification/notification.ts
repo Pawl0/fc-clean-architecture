14e6bf063b4fd7a83b6a22409356676fb4ac033c
Paulo Henrique Oliveira dos Santos <paulo.hosantos@einstein.br>
2023-04-14 17:40:07 -0300

    feat: notification pattern

--- src/domain/@shared/notification/notification.ts
+++ src/domain/@shared/notification/notification.ts
@@ -1,7 +1,7 @@
 export type NotificationErrorProps = {
   message: string;
   context: string;
-};
+}
 
 export default class Notification {
   private errors: NotificationErrorProps[] = [];
@@ -20,10 +20,9 @@ export default class Notification {
 
   messages(context?: string): string {
     let message = "";
-    this.errors.forEach((error) => {
-      if (context === undefined || error.context === context) {
+    this.errors.forEach(error => {
+      if (context === undefined || error.context === context)
         message += `${error.context}: ${error.message},`;
-      }
     });
     return message;
   }
